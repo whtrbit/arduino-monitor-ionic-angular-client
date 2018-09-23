b580e8a85b7c123263e78748cfc5050c58ad59aa
whtrbit <kontakt@grzegorzbielak.pl>
2018-09-23 21:56:37 +0200

    Prepare for Android deploy.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+    firebase: {
+        apiKey: 'AIzaSyBT8EKZoJX1h4RUp_6eBb1Kt2DXb5ir4Io',
+        authDomain: 'arduino-monitor-7a5c5.firebaseapp.com',
+        databaseURL: 'https://arduino-monitor-7a5c5.firebaseio.com',
+        projectId: 'arduino-monitor-7a5c5',
+        storageBucket: 'arduino-monitor-7a5c5.appspot.com',
+        messagingSenderId: '258745019678'
+    }
 };
